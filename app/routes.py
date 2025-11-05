34a15a0841536270fd74752f94b9c16421bfc377
Siméon0477 <nouhoumimeon01@gmail.com>
2025-11-05 05:18:50 +0100

    json file

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify, render_template
 import pickle as pkl 
 from app import app
+import json
 
-model = pkl.load(open("./models/model.pkl", "rb"))
+model = pkl.load(open("{{url_for('models', filename='model.pkl')}}", "rb"))
 
 #point final par défaut
 @app.route('/')
 def home():
-    return render_template('index.html')
+    with open("{{url_for('static', filename='style.css')}}", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return render_template('index.html', data=data)
 
 #définition du point final de la prédication
 @app.route('/predict', methods=['POST'])
